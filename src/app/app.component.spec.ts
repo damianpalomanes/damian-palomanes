ccc5ffa887dcd31a5b2390c09d287f844105519f
Damian Palomanes <damianapa@ext.inditex.com>
2023-04-27 14:19:15 +0200

    Initial committ

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'your-personal-website'`, () => {
+  it(`should have as title 'damian-palomanes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('your-personal-website');
+    expect(app.title).toEqual('damian-palomanes');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('your-personal-website app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('damian-palomanes app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'your-personal-website';
+  title = 'damian-palomanes';
 }
